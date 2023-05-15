b5490f3be90a8d22aadbb0b395aae773ac0a67f2
SharmagRit <sharmagritstuff@gmail.com>
2023-05-15 21:02:57 +0530

    phoenix: Add GoogleCameraGo
     * from: https://t.me/GcamGo
    
    Signed-of-by: SharmagRit <sharmagritstuff@gmail.com>

--- device.mk
+++ device.mk
@@ -25,6 +25,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_overlay_static.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.phoenix.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.phoenix.rc
